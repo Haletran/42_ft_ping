223d91ccd2913613ac68178f75bce535b8391c9a
Baptiste-Pasquier <bpasquier123@gmail.com>
2025-03-10 17:55:33 +0000

    add: some functions for later and cleaner approach to fix leaks

--- src/main.c
+++ src/main.c
@@ -31,7 +31,7 @@ bool get_IP(char **argv, t_token *parse)
         if (argv[i][0] != '-')
         {
             parse->ip = argv[i];
-            parse->id = rand() % (9999 - 1000 + 1);
+            parse->id = rand() % (9999 - 1000 + 1); // not really what i should do but ok for now
             return true;
         }
     }
@@ -41,6 +41,8 @@ bool get_IP(char **argv, t_token *parse)
 
 int free_all(t_data *data, t_token *parse, int exit_code)
 {
+    if (exit_code == 1)
+        perror("Error: ");
     if (data)
         free(data);
     if (parse)
@@ -59,7 +61,7 @@ int main(int argc, char **argv)
     data = malloc(1000);
     parse = malloc(1000);
     srand(time(NULL));
-    struct sockaddr_in *servaddr;
+    struct sockaddr_in *servaddr = malloc(1000);
 
     data->vflag = false;
     data->sock_len = sizeof(struct sockaddr_in);
@@ -93,27 +95,50 @@ int main(int argc, char **argv)
     else
         printf("\n");
 
-    data->sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    data->sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
     if (!data->sockfd)
         exit(free_all(data, parse, 1));
 
-    //  IP_HDRINCL
     const int on = 1;
     char buffer[1024];
     char recvline[1000];
+
+    memset(servaddr, 0, sizeof(struct sockaddr_in));
+    servaddr->sin_family = AF_INET;
+    servaddr->sin_addr.s_addr = inet_addr(parse->ip);
+
     // setsockopt to setup the socket timeout and some other stuff
     setsockopt(data->sockfd, IPPROTO_IP, IP_HDRINCL, &on, sizeof(on));
+    bzero(buffer, sizeof(buffer));
 
+    // setup struct of recv so that i can access to request
+    struct sockaddr_in recv_addr;
+    memset(&recv_addr, 0, sizeof(recv_addr));
+    socklen_t recv_len = sizeof(recv_addr);
+    recv_addr.sin_addr.s_addr = inet_addr(parse->ip);
+
+    // SEND ICMP REQUEST
     while (run_ping)
     {
         // sendto to send the ICMP ECHOREQUEST
         sendto(data->sockfd, buffer, strlen(buffer), 0, (const struct sockaddr *)servaddr, data->sock_len);
+
         // recvfrom to receive the ECHO REPLY
-        n = recvfrom(data->sockfd, recvline, 1000, 0, 0, NULL);
+        n = recvfrom(data->sockfd, recvline, sizeof(recvline), 0,
+                     (struct sockaddr *)&recv_addr, &recv_len);
+
+        printf("%d bytes from %s: \n", n, inet_ntoa(recv_addr.sin_addr));
+        if (n < 0)
+            exit(free_all(data, parse, 1));
+        sleep(1);
+
+        // PARSE ICMP REQUEST TO PRINT THE RESULT
+        // Here is the output : 64 bytes from wildebeest1p.gnu.org (209.51.188.116): icmp_seq=1 ttl=54 time=107 ms
     }
 
     // Example final print even if the while is stopped:
     // --- gnu.org ping statistics ---
     // 3 packets transmitted, 0 received, 100% packet loss, time 2032ms
+    free_all(data, parse, 0);
     return (0);
 }
